cef1f9884734e232fc3680304587d408b9e1d754
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-13 09:20:06 +0200

    Added a key selector to the members binder component.

--- TryAtSoftware.Extensions.Reflection/MembersBinder.cs
+++ TryAtSoftware.Extensions.Reflection/MembersBinder.cs
@@ -11,20 +11,35 @@ using TryAtSoftware.Extensions.Reflection.Interfaces;
 public class MembersBinder<TEntity> : IMembersBinder<TEntity>
 {
     public MembersBinder([CanBeNull] Func<MemberInfo, bool> isValid, BindingFlags bindingFlags)
+        : this(isValid, keySelector: null, bindingFlags)
     {
-        var members = GetMembers(typeof(TEntity), isValid, bindingFlags);
+    }
+    
+    public MembersBinder([CanBeNull] Func<MemberInfo, bool> isValid, [CanBeNull] Func<MemberInfo, string> keySelector, BindingFlags bindingFlags)
+    {
+        var members = GetMembers(typeof(TEntity), isValid, bindingFlags, keySelector);
         this.MemberInfos = new ReadOnlyDictionary<string, MemberInfo>(members);
     }
 
     public IReadOnlyDictionary<string, MemberInfo> MemberInfos { get; }
 
-    private static Dictionary<string, MemberInfo> GetMembers([NotNull] IReflect type, [CanBeNull] Func<MemberInfo, bool> isValid, BindingFlags bindingFlags)
+    private static Dictionary<string, MemberInfo> GetMembers([NotNull] IReflect type, [CanBeNull] Func<MemberInfo, bool> isValid, BindingFlags bindingFlags, [CanBeNull] Func<MemberInfo, string> keySelector)
     {
         var membersDict = new Dictionary<string, MemberInfo>();
         var members = type.GetMembers(bindingFlags).SafeWhere(isValid);
 
         foreach (var member in members)
-            membersDict[member.Name] = member;
+        {
+            string key;
+            if (keySelector is null) key = member.Name;
+            else 
+            {
+                key = keySelector(member);
+                if (string.IsNullOrWhiteSpace(key)) continue;
+            }
+
+            membersDict[key] = member;
+        }
 
         return membersDict;
     }

3f28f7c96027fa959c34657c7a0922ad0bb80c26
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-16 08:52:08 +0200

    Wrote documentation for all extension methods and components within the `TryAtSoftware.Extensions.Reflection` library.

--- TryAtSoftware.Extensions.Reflection/MembersBinderExtensions.cs
+++ TryAtSoftware.Extensions.Reflection/MembersBinderExtensions.cs
@@ -1,18 +1,29 @@
-﻿namespace TryAtSoftware.Extensions.Reflection;
+namespace TryAtSoftware.Extensions.Reflection;
 
 using System;
 using System.Reflection;
 using TryAtSoftware.Extensions.Reflection.Interfaces;
 
+/// <summary>
+/// A static class containing standard extension methods applicable to any <see cref="IMembersBinder"/> instance.
+/// </summary>
 public static class MembersBinderExtensions
 {
-    public static MemberInfo GetRequiredMemberInfo(this IMembersBinder membersBinder, string name)
+    /// <summary>
+    /// Use this method to retrieve a required member from the extended <paramref name="membersBinder"/> instance.
+    /// </summary>
+    /// <param name="membersBinder">The extended <see cref="IMembersBinder"/> instance.</param>
+    /// <param name="key">The key of the required member info.</param>
+    /// <returns>Returns the requested member info from the extended <paramref name="membersBinder"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the extended <paramref name="membersBinder"/> is null or the provided <paramref name="key"/> is null or empty.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if the extended <paramref name="membersBinder"/> does not contain a member mpped against the requested <paramref name="key"/>.</exception>
+    public static MemberInfo GetRequiredMemberInfo(this IMembersBinder membersBinder, string key)
     {
         if (membersBinder is null) throw new ArgumentNullException(nameof(membersBinder));
-        if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
+        if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
 
-        if (!membersBinder.MemberInfos.TryGetValue(name, out var memberInfo))
-            throw new InvalidOperationException($"Member {name} not found in {membersBinder.Type.Name}");
+        if (!membersBinder.MemberInfos.TryGetValue(key, out var memberInfo))
+            throw new InvalidOperationException($"Member {key} not found in {TypeNames.Get(membersBinder.Type)}");
 
         return memberInfo;
     }
--- TryAtSoftware.Extensions.Reflection/TypeNames.cs
+++ TryAtSoftware.Extensions.Reflection/TypeNames.cs
@@ -5,11 +5,23 @@ using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 
+/// <summary>
+/// A static class responsible for providing beautified type names.
+/// </summary>
+/// <remarks>
+/// Internally, there is a cache so multiple calls to the same method will not be slow.
+/// </remarks>
 public static class TypeNames
 {
     private const char GenericTypeNameSeparator = '`';
     private static readonly ConcurrentDictionary<Type, string> _memo = new();
 
+    /// <summary>
+    /// Use this method to get the beutified name of the requested <paramref name="type"/>.
+    /// </summary>
+    /// <param name="type">The <see cref="Type"/> for which a beutified name should be generated and returned.</param>
+    /// <returns>Returns the subsequently generated beautified name of the provided <paramref name="type"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided <paramref name="type"/> is null.</exception>
     public static string Get(Type type)
     {
         if (type is null) throw new ArgumentNullException(nameof(type));
@@ -42,7 +54,17 @@ public static class TypeNames
     }
 }
 
+/// <summary>
+/// A static class responsible for providing beautified type names.
+/// </summary>
+/// <typeparam name="T">The type for which a beautified name is requested.</typeparam>
+/// <remarks>
+/// Internally, the non-generic <see cref="TypeNames"/> class is used.
+/// </remarks>
 public static class TypeNames<T>
 {
+    /// <summary>
+    /// Gets the beautified name of <typeparamref name="T"/>.
+    /// </summary>
     public static string Value => TypeNames.Get(typeof(T));
 }

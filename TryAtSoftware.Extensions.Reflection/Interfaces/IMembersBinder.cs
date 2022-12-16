66ed79f985612add29e07f6a71c570b35a8570fc
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-16 08:43:28 +0200

    Wrote documentation for most of the extension methods and components within the `TryAtSoftware.Extensions.Reflection` package.

--- TryAtSoftware.Extensions.Reflection/Interfaces/IMembersBinder.cs
+++ TryAtSoftware.Extensions.Reflection/Interfaces/IMembersBinder.cs
@@ -4,8 +4,18 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
+/// <summary>
+/// An interface defining the structure of a component responsible for discovering and caching information about part of the members of a given type.
+/// </summary>
 public interface IMembersBinder
 {
+    /// <summary>
+    /// Gets the type used by this member binder.
+    /// </summary>
     Type Type { get; }
+
+    /// <summary>
+    /// Gets an <see cref="IReadOnlyDictionary{TKey, TValue}"/> where all discovered <see cref="MemberInfo"/> instances are mapped appropriately to a given key.
+    /// </summary>
     IReadOnlyDictionary<string, MemberInfo> MemberInfos { get; }
 }

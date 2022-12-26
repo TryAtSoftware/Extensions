3a470167d391d9908250fd310f96ea83110eb3e5
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-26 11:26:18 +0200

    Wrote tests for the getter expression construction

--- TryAtSoftware.Extensions.Reflection.Tests/ExpressionsExtensionsTests.cs
+++ TryAtSoftware.Extensions.Reflection.Tests/ExpressionsExtensionsTests.cs
@@ -15,7 +15,7 @@ public class ExpressionsExtensionsTests
 
     [Fact]
     public void MemberInfoShouldBeSuccessfullyRetrieved() => AssertMemberInfoRetrieval<Person, string>(p => p.FirstName, typeof(Person), nameof(Person.FirstName));
-    
+
     [Fact]
     public void MemberInfoShouldBeSuccessfullyRetrievedFromDerivedClasses() => AssertMemberInfoRetrieval<Student, string>(p => p.FirstName, typeof(Person), nameof(Person.FirstName));
 
@@ -40,19 +40,19 @@ public class ExpressionsExtensionsTests
         var firstName = firstNameAccessor(person);
         Assert.Equal(person.FirstName, firstName);
     }
-    
+
     [Fact]
     public void AccessorShouldBeSuccessfullyConstructedForInaccessibleProperties()
     {
-        var inaccessiblePropertyAccessor = GetCompiledPropertyAccessor<Person, string>("InaccessibleProperty", BindingFlags.NonPublic);
+        var inaccessiblePropertyAccessor = GetCompiledPropertyAccessor<ExpressionTestsClass, string>("InaccessibleProperty", BindingFlags.NonPublic);
 
-        var personRandomizer = new PersonRandomizer();
-        var person = personRandomizer.PrepareRandomValue();
+        var inaccessiblePropertyValue = RandomizationHelper.GetRandomString();
+        var testInstance = new ExpressionTestsClass { InaccessiblePropertySetter = inaccessiblePropertyValue };
 
-        var inaccessibleValue = inaccessiblePropertyAccessor(person);
-        Assert.Equal("You cannot access this value", inaccessibleValue);
+        var inaccessibleValue = inaccessiblePropertyAccessor(testInstance);
+        Assert.Equal(inaccessiblePropertyValue, inaccessibleValue);
     }
-    
+
     [Fact]
     public void PropertyAccessorShouldBeSuccessfullyConstructedWhenConversionIsNecessary()
     {
@@ -75,13 +75,22 @@ public class ExpressionsExtensionsTests
     }
 
     [Fact]
+    public void PropertyAccessorShouldNotBeConstructedIfItIsNotReadable()
+    {
+        var inaccessiblePropertySetter = typeof(ExpressionTestsClass).GetProperty(nameof(ExpressionTestsClass.InaccessiblePropertySetter));
+        Assert.NotNull(inaccessiblePropertySetter);
+
+        Assert.Throws<InvalidOperationException>(() => inaccessiblePropertySetter.ConstructPropertyAccessor<ExpressionTestsClass, string>());
+    }
+
+    [Fact]
     public void PropertySetterShouldBeConstructedSuccessfully()
     {
         var firstNameSetter = GetCompiledPropertySetter<Person, string>(nameof(Person.FirstName));
-        
+
         var person = new Person();
         var randomString = RandomizationHelper.GetRandomString();
-        
+
         firstNameSetter(person, randomString);
         Assert.Equal(randomString, person.FirstName);
     }
@@ -90,14 +99,32 @@ public class ExpressionsExtensionsTests
     public void PropertySetterShouldBeConstructedSuccessfullyWhenConversionIsRequired()
     {
         var firstNameSetter = GetCompiledPropertySetter<Person, byte>(nameof(Person.Age));
-        
+
         var person = new Person();
         var randomByte = (byte)RandomizationHelper.RandomInteger(byte.MinValue, byte.MaxValue);
-        
+
         firstNameSetter(person, randomByte);
         Assert.Equal(randomByte, person.Age);
     }
-    
+
+    [Fact]
+    public void PropertySetterShouldNotBeConstructedIfTheReflectedTypeDoesNotCorrespondToTheProvidedGenericTypeParameter()
+    {
+        var firstNameProperty = typeof(Student).GetProperty(nameof(Student.FirstName));
+        Assert.NotNull(firstNameProperty);
+
+        Assert.Throws<InvalidOperationException>(() => firstNameProperty.ConstructPropertySetter<Person, string>());
+    }
+
+    [Fact]
+    public void PropertySetterShouldNotBeConstructedIfItIsReadOnly()
+    {
+        var inaccessiblePropertyGetter = typeof(ExpressionTestsClass).GetProperty(nameof(ExpressionTestsClass.InaccessiblePropertyGetter));
+        Assert.NotNull(inaccessiblePropertyGetter);
+
+        Assert.Throws<InvalidOperationException>(() => inaccessiblePropertyGetter.ConstructPropertySetter<ExpressionTestsClass, string>());
+    }
+
     private static void AssertMemberInfoRetrieval<T, TValue>(Expression<Func<T, TValue>> selector, Type declaringType, string memberName)
     {
         var memberInfo = selector.GetMemberInfo();
@@ -110,7 +137,7 @@ public class ExpressionsExtensionsTests
 
         var propertyInfo = typeof(T).GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | additionalBindingFlags);
         Assert.NotNull(propertyInfo);
-        
+
         var propertyAccessor = propertyInfo.ConstructPropertyAccessor<T, TValue>();
         Assert.NotNull(propertyAccessor);
 
@@ -126,7 +153,7 @@ public class ExpressionsExtensionsTests
 
         var propertyInfo = typeof(T).GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | additionalBindingFlags);
         Assert.NotNull(propertyInfo);
-        
+
         var propertySetter = propertyInfo.ConstructPropertySetter<T, TValue>();
         Assert.NotNull(propertySetter);
 
@@ -134,5 +161,12 @@ public class ExpressionsExtensionsTests
         Assert.NotNull(compiledPropertySetter);
 
         return compiledPropertySetter;
+    }
+
+    private class ExpressionTestsClass
+    {
+        private string InaccessibleProperty { get; set; }
+        public string InaccessiblePropertySetter { set => this.InaccessibleProperty = value; }
+        public string InaccessiblePropertyGetter { get => this.InaccessibleProperty; }
     }
 }

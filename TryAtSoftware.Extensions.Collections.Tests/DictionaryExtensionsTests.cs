2b1c017e1580b66ef94cb168cb62eb2099e2c8fb
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-27 13:17:26 +0200

    Added the `EnsureValue` and `AsReadOnlyDictionary` extension methods

--- TryAtSoftware.Extensions.Collections.Tests/DictionaryExtensionsTests.cs
+++ TryAtSoftware.Extensions.Collections.Tests/DictionaryExtensionsTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TryAtSoftware.Randomizer.Core.Helpers;
 using Xunit;
 
 public class DictionaryExtensionsTests
@@ -11,8 +12,8 @@ public class DictionaryExtensionsTests
     [Fact]
     public void OrEmptyIfNullShouldReturnTheSameDictionaryIfItIsNotNull()
     {
-        var collection = TestsHelper.GetStandardCollection();
-        var dictionary = TestsHelper.GetElementsMap(collection);
+        var standardCollection = TestsHelper.GetStandardCollection();
+        var dictionary = TestsHelper.GetElementsMap(standardCollection);
 
         var result = dictionary.OrEmptyIfNull();
         Assert.NotNull(result);
@@ -25,5 +26,50 @@ public class DictionaryExtensionsTests
         var result = ((IDictionary<object, object>?)null).OrEmptyIfNull();
         Assert.NotNull(result);
         Assert.Empty(result);
+    }
+
+    [Fact]
+    public void EnsureValueShouldThrowIfTheExtendedDictionaryIsNull()
+        => Assert.Throws<ArgumentNullException>(() => ((IDictionary<object, object>)null!).EnsureValue(1));
+
+    [Fact]
+    public void EnsureValueShouldHandleExistingValues()
+    {
+        var dictionary = new Dictionary<int, List<int>>();
+        var key = RandomizationHelper.RandomInteger(1, 100);
+        var value = new List<int>();
+        dictionary[key] = value;
+
+        var ensuredValue = dictionary.EnsureValue(key);
+        Assert.Same(value, ensuredValue);
+    }
+
+    [Fact]
+    public void EnsureValueShouldHandleNonExistingValues()
+    {
+        var dictionary = new Dictionary<int, List<int>>();
+        var key = RandomizationHelper.RandomInteger(1, 100);
+
+        var ensuredValue = dictionary.EnsureValue(key);
+        Assert.NotNull(ensuredValue);
+        Assert.Empty(ensuredValue);
+    }
+
+    [Fact]
+    public void AsReadOnlyDictionaryShouldHandleNull()
+    {
+        var result = ((IDictionary<object, object>?)null).AsReadOnlyDictionary();
+        Assert.NotNull(result);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void AsReadOnlyCollectionShouldWorkCorrectly()
+    {
+        var standardCollection = TestsHelper.GetStandardCollection();
+        var standardDictionary = TestsHelper.GetElementsMap(standardCollection);
+        var readonlyDictionary = standardDictionary.AsReadOnlyDictionary();
+
+        Assert.Equal(standardDictionary, readonlyDictionary);
     }
 }
--- TryAtSoftware.Extensions.Collections/DictionaryExtensions.cs
+++ TryAtSoftware.Extensions.Collections/DictionaryExtensions.cs
@@ -1,6 +1,8 @@
-﻿namespace TryAtSoftware.Extensions.Collections;
+namespace TryAtSoftware.Extensions.Collections;
 
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 /// <summary>
 /// A static class containing standard extension methods applicable to any <see cref="IDictionary{TKey,TValue}"/> instance.
@@ -14,5 +16,39 @@ public static class DictionaryExtensions
     /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
     /// <param name="dictionary">The extended <see cref="IDictionary{TKey,TValue}"/> instance.</param>
     /// <returns>Returns the same dictionary if it was not null. Else, returns an new empty dictionary.</returns>
-    public static IDictionary<TKey, TValue> OrEmptyIfNull<TKey, TValue>(this IDictionary<TKey, TValue>? dictionary) => dictionary ?? new Dictionary<TKey, TValue>(capacity: 0);
+    public static IDictionary<TKey, TValue> OrEmptyIfNull<TKey, TValue>(this IDictionary<TKey, TValue>? dictionary)
+        where TKey : notnull
+        => dictionary ?? new Dictionary<TKey, TValue>(capacity: 0);
+
+    /// <summary>
+    /// Use this method to ensure that a value within a dictionary exists for the requested key.
+    /// If such does not exist, a new instance will be created and stored for the requested key.
+    /// </summary>
+    /// <param name="dictionary">The extended <see cref="IDictionary{TKey, TValue}"/> instance.</param>
+    /// <param name="key">The key used to lookup whether or not such value exists in the requested <paramref name="dictionary"/>.</param>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+    /// <returns>Returns the value for the requested key.</returns>
+    public static TValue EnsureValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        where TKey : notnull
+        where TValue : class, new()
+    {
+        if (dictionary is null) throw new ArgumentNullException(nameof(dictionary));
+        if (dictionary.TryGetValue(key, out var value)) return value;
+
+        var newValueInstance = new TValue();
+        dictionary[key] = newValueInstance;
+        return newValueInstance;
+    }
+
+    /// <summary>
+    /// Use this method to construct an <see cref="IReadOnlyDictionary{TKey, TValue}"/> instance from the extended <paramref name="dictionary"/>.
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+    /// <param name="dictionary">The extended <see cref="IDictionary{TKey, TValue}"/> instance.</param>
+    /// <returns>Returns an <see cref="IReadOnlyDictionary{TKey, TValue}"/> containing all elements from the extended <paramref name="dictionary"/>.</returns>
+    public static IReadOnlyDictionary<TKey, TValue> AsReadOnlyDictionary<TKey, TValue>(this IDictionary<TKey, TValue>? dictionary)
+        where TKey : notnull
+        => new ReadOnlyDictionary<TKey, TValue>(dictionary.OrEmptyIfNull());
 }

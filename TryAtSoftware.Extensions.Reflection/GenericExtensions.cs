cd16cfff176b456fcf6c76dfcfa644b4d089e140
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-28 11:12:29 +0200

    Added documentation for the new extension methods.

--- TryAtSoftware.Extensions.Reflection/GenericExtensions.cs
+++ TryAtSoftware.Extensions.Reflection/GenericExtensions.cs
@@ -4,14 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class TypeExtensions
+/// <summary>
+/// A static class containing standard extension methods that are useful when working with reflection over generic types or methods.
+/// </summary>
+public static class GenericExtensions
 {
     /// <summary>
-    /// Use this method to extract in a dictionary the generic types setup for a given type.
+    /// Use this method to extract in a dictionary the setup of generic parameters for a given type.
     /// </summary>
     /// <param name="type">The type used to extract the generic types setup from.</param>
-    /// <param name="typesConfiguration">A one-to-one mapping of "attribute type" and "generic argument type".</param>
-    /// <returns>Returns a dictionary containing the specific types for each of the generic arguments against their names.</returns>
+    /// <param name="typesConfiguration">A one-to-one mapping of "attribute type" and "generic parameter type".</param>
+    /// <returns>Returns a dictionary containing the specific types for each of the generic parameters against their names.</returns>
     /// <remarks>
     /// Imagine we have the following type definition:
     /// public class MyType&lt;[KeyType] TKey, [ValueType] TValue&gt; {}
@@ -19,6 +22,10 @@ public static class TypeExtensions
     /// ExtractGenericParametersSetup(typeof(MyType), { typeof(KeyTypeAttribute): typeof(int), typeof(ValueTypeAttribute): typeof(string) })
     /// should return { "TKey": typeof(int), "TValue": typeof(string) }
     /// </remarks>
+    /// <exception cref="ArgumentNullException">Thrown if the extended <paramref name="type"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if the <paramref name="typesConfiguration"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if there are none or more than one attributes for any of the unresolved generic parameters.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if there is no configured type for the attribute a generic parameter is decorated with.</exception>
     public static IDictionary<string, Type> ExtractGenericParametersSetup(this Type type, IDictionary<Type, Type> typesConfiguration)
     {
         if (type is null) throw new ArgumentNullException(nameof(type));
@@ -44,12 +51,13 @@ public static class TypeExtensions
     }
 
     /// <summary>
-    /// 
+    /// Use this method to make the extended <paramref name="type"/> generic using a parameters setup.
     /// </summary>
-    /// <param name="type"></param>
-    /// <param name="genericParametersSetup"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
+    /// <param name="type">The type that should be made generic.</param>
+    /// <param name="genericParametersSetup">A dictionary containing the specific types for each of the generic parameters against their names.</param>
+    /// <returns>Returns the generic version of the extended <paramref name="type"/> where each generic parameter is substituted with a specific type from the <paramref name="genericParametersSetup"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the extended <paramref name="type"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if the <paramref name="genericParametersSetup"/> is null.</exception>
     public static Type MakeGenericType(this Type type, IDictionary<string, Type> genericParametersSetup)
     {
         if (type is null) throw new ArgumentNullException(nameof(type));

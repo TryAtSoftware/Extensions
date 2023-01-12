2625581da700169acb96e94d0440055377969fa8
Tony Troeff <tonyslav_troeff@outlook.com>
2023-01-12 19:31:55 +0200

    Added a documentation and extended the validation of the method so that constructors of abstract classes should not be processed.

--- TryAtSoftware.Extensions.Reflection/ExpressionsExtensions.cs
+++ TryAtSoftware.Extensions.Reflection/ExpressionsExtensions.cs
@@ -85,10 +85,20 @@ public static class ExpressionsExtensions
         return Expression.Lambda<Action<T, TValue>>(assignExpression, instanceParameter, valueParameter);
     }
 
+    /// <summary>
+    /// Use this method in order to construct an <see cref="Expression"/> for instantiation an object of type <typeparamref name="T"/> using the requested <paramref name="constructorInfo"/>.
+    /// </summary>
+    /// <param name="propertyInfo">The <see cref="ConstructorInfo"/> describing the constructor that should be used during the instantiation process.</param>
+    /// <typeparam name="T">The type of object to be instantiated (should be equal to the reflected type of the extended <paramref name="constructorInfo"/>).</typeparam>
+    /// <returns>Returns a subsequently built expression for constructing a new instance of type <typeparamref name="T"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided <paramref name="constructorInfo"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if the <see cref="MemberInfo.ReflectedType"/> of the provided <paramref name="constructorInfo"/> does not match <typeparamref name="T"/>.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if the provided <paramref name="constructorInfo"/> is a constructor of an abstract class.</exception>
     public static Expression<Func<object?[], T>> ConstructObjectInitializer<T>(this ConstructorInfo constructorInfo)
     {
         if (constructorInfo is null) throw new ArgumentNullException(nameof(constructorInfo));
         ValidateCorrectReflectedType(constructorInfo, typeof(T));
+        if (constructorInfo.DeclaringType.IsAbstract) throw new InvalidOperationException("This is a constructor of an abstract class.");
 
         var argumentsParameter = Expression.Parameter(typeof(object?[]));
 

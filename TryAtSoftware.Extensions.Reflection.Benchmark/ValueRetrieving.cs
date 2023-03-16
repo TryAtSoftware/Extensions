e9eb1987f163acc21be64131f665c646fa9b7bb7
Tony Troeff <tonyslav_troeff@outlook.com>
2023-03-16 10:52:34 +0200

    Added sample results from the benchmark execution

--- TryAtSoftware.Extensions.Reflection.Benchmark/ValueRetrieving.cs
+++ TryAtSoftware.Extensions.Reflection.Benchmark/ValueRetrieving.cs
@@ -3,13 +3,28 @@
 using System.Reflection;
 using BenchmarkDotNet.Attributes;
 
+/*
+16/03/2023, TryAtSoftware.Extensions.Reflection v1.1.1-alpha.2
+|                               Method |       N |         Mean |     Error |    StdDev |
+|------------------------------------- |-------- |-------------:|----------:|----------:|
+| RetrievePropertyValueUsingReflection |   10000 |    144.56 us |  2.799 us |  3.223 us |
+| RetrievePropertyValueUsingExpression |   10000 |     11.44 us |  0.061 us |  0.057 us |
+| RetrievePropertyValueUsingReflection |   50000 |    705.24 us |  6.863 us |  6.420 us |
+| RetrievePropertyValueUsingExpression |   50000 |     56.75 us |  0.224 us |  0.175 us |
+| RetrievePropertyValueUsingReflection |  100000 |  1,410.36 us |  9.666 us |  8.569 us |
+| RetrievePropertyValueUsingExpression |  100000 |    113.54 us |  0.673 us |  0.562 us |
+| RetrievePropertyValueUsingReflection |  500000 |  7,187.74 us | 96.072 us | 80.225 us |
+| RetrievePropertyValueUsingExpression |  500000 |    567.66 us |  3.361 us |  3.144 us |
+| RetrievePropertyValueUsingReflection | 1000000 | 13,973.26 us | 77.321 us | 72.326 us |
+| RetrievePropertyValueUsingExpression | 1000000 |  1,149.74 us | 21.910 us | 23.444 us |
+ */
 public class ValueRetrieving
 {
     private readonly BenchmarkModel _benchmarkModel = new () { Value = 1024 };
     private PropertyInfo _propertyInfo = null!;
     private Func<BenchmarkModel, int> _compiledExpression = null!;
 
-    [Params(100, 1_000, 5_000, 10_000, 50_000, 100_000, 500_000, 1_000_000)] public int N { get; set; }
+    [Params(10_000, 50_000, 100_000, 500_000, 1_000_000)] public int N { get; set; }
 
     [GlobalSetup]
     public void Setup()
--- TryAtSoftware.Extensions.Reflection.Benchmark/ValueSetting.cs
+++ TryAtSoftware.Extensions.Reflection.Benchmark/ValueSetting.cs
@@ -3,13 +3,28 @@
 using System.Reflection;
 using BenchmarkDotNet.Attributes;
 
+/*
+16/03/2023, TryAtSoftware.Extensions.Reflection v1.1.1-alpha.2
+|                          Method |       N |         Mean |      Error |       StdDev |       Median |
+|-------------------------------- |-------- |-------------:|-----------:|-------------:|-------------:|
+| SetPropertyValueUsingReflection |   10000 |    327.84 us |   1.940 us |     1.720 us |    327.93 us |
+| SetPropertyValueUsingExpression |   10000 |     11.37 us |   0.035 us |     0.031 us |     11.37 us |
+| SetPropertyValueUsingReflection |   50000 |  1,561.69 us |  17.282 us |    14.431 us |  1,559.26 us |
+| SetPropertyValueUsingExpression |   50000 |     56.73 us |   0.629 us |     0.588 us |     56.39 us |
+| SetPropertyValueUsingReflection |  100000 |  3,072.71 us |   9.992 us |     8.344 us |  3,073.82 us |
+| SetPropertyValueUsingExpression |  100000 |    113.21 us |   0.927 us |     0.774 us |    112.92 us |
+| SetPropertyValueUsingReflection |  500000 | 15,677.52 us | 105.741 us |    98.910 us | 15,662.07 us |
+| SetPropertyValueUsingExpression |  500000 |    566.84 us |   3.425 us |     3.036 us |    565.96 us |
+| SetPropertyValueUsingReflection | 1000000 | 31,877.39 us | 635.977 us | 1,146.798 us | 31,247.00 us |
+| SetPropertyValueUsingExpression | 1000000 |  1,131.43 us |   3.365 us |     2.983 us |  1,130.91 us |
+ */
 public class ValueSetting
 {
     private readonly BenchmarkModel _benchmarkModel = new () { Value = 1024 };
     private PropertyInfo _propertyInfo = null!;
     private Action<BenchmarkModel, int> _compiledExpression = null!;
 
-    [Params(100, 1_000, 5_000, 10_000, 50_000, 100_000, 500_000, 1_000_000)] public int N { get; set; }
+    [Params(10_000, 50_000, 100_000, 500_000, 1_000_000)] public int N { get; set; }
 
     [GlobalSetup]
     public void Setup()

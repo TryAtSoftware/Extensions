4fc4548609f04e1c35da4a110091d77838d29e11
Tony Troeff <tonyslav_troeff@outlook.com>
2023-05-28 22:26:28 +0300

    Added one more test validating correct attribute scan for properties and correct error handling.

--- TryAtSoftware.Extensions.Reflection.Tests/HierarchyScannerTests.cs
+++ TryAtSoftware.Extensions.Reflection.Tests/HierarchyScannerTests.cs
@@ -1,11 +1,17 @@
 ﻿namespace TryAtSoftware.Extensions.Reflection.Tests;
 
 using System;
-using TryAtSoftware.Extensions.Reflection.Interfaces;
 using Xunit;
 
 public class HierarchyScannerTests
 {
+    [Fact]
+    public void ExceptionShouldBeThrownIfAttributeShouldBeScannedForNullMember()
+    {
+        var hierarchyScanner = new HierarchyScanner();
+        Assert.Throws<ArgumentNullException>(() => hierarchyScanner.ScanForAttribute<IdAttribute>(null!));
+    }
+    
     [Theory]
     [InlineData(typeof(ClassA), nameof(ClassA.Method), 1)]
     [InlineData(typeof(ClassB), nameof(ClassB.Method), 2)]
@@ -25,9 +31,29 @@ public class HierarchyScannerTests
         Assert.NotNull(attribute);
         Assert.Equal(expectedId, attribute.Id);
     }
+
+    [Theory]
+    [InlineData(typeof(ClassA), nameof(ClassA.Property), 1)]
+    [InlineData(typeof(ClassB), nameof(ClassB.Property), 2)]
+    [InlineData(typeof(ClassC), nameof(ClassC.Property), 1)]
+    [InlineData(typeof(ClassD), nameof(ClassD.Property), 3)]
+    [InlineData(typeof(ClassE), nameof(ClassE.Property), 4)]
+    [InlineData(typeof(ClassF), nameof(ClassF.Property), 3)]
+    public void PropertyAttributeShouldBeSuccessfullyScanned(Type type, string propertyName, int expectedId)
+    {
+        var decoratedProperty = type.GetProperty(propertyName);
+        Assert.NotNull(decoratedProperty);
+
+        var hierarchyScanner = new HierarchyScanner();
+        var scannedAttributes = hierarchyScanner.ScanForAttribute<IdAttribute>(decoratedProperty);
+
+        var attribute = Assert.Single(scannedAttributes);
+        Assert.NotNull(attribute);
+        Assert.Equal(expectedId, attribute.Id);
+    }
 }
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
 file class IdAttribute : Attribute
 {
     public int Id { get; set; }
@@ -36,23 +62,32 @@ file class IdAttribute : Attribute
 file class ClassA
 {
     [Id(Id = 1)]
+    public virtual string Property => throw new NotImplementedException();
+    
+    [Id(Id = 1)]
     public virtual void Method() => throw new NotImplementedException();
 }
 
 file class ClassB : ClassA
 {
     [Id(Id = 2)]
+    public override string Property => throw new NotImplementedException();
+
+    [Id(Id = 2)]
     public override void Method() => throw new NotImplementedException();
 }
 
 file class ClassC : ClassA
 {
+    public override string Property => throw new NotImplementedException();
+
     public override void Method() => throw new NotImplementedException();
 }
 
 [Id(Id = 3)]
 file class ClassD
 {
+    public string Property => throw new NotImplementedException();
     public void Method() => throw new NotImplementedException();
 }
 

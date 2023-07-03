387a5ab90764f4af822bb3eb65f8c56b740f551e
Tony Troeff <tonyslav_troeff@outlook.com>
2023-07-03 23:16:03 +0300

    Documented the new `IAssemblyLoader` interface.

--- TryAtSoftware.Extensions.Reflection/Interfaces/IAssemblyLoader.cs
+++ TryAtSoftware.Extensions.Reflection/Interfaces/IAssemblyLoader.cs
@@ -2,7 +2,15 @@
 
 using System.Reflection;
 
+/// <summary>
+/// An interface defining the structure of a component responsible for loading assemblies.
+/// </summary>
 public interface IAssemblyLoader
 {
+    /// <summary>
+    /// Use this method to load an assembly given its name.
+    /// </summary>
+    /// <param name="assemblyName">The object that describes the assembly to be loaded.</param>
+    /// <returns>The loaded assembly.</returns>
     Assembly? Load(AssemblyName assemblyName);
 }

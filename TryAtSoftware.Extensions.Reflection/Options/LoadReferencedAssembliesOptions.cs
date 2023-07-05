099df4e9995cf5c8d0f4fa45e8254b4f4584588a
Tony Troeff <tonyslav_troeff@outlook.com>
2023-07-05 23:10:09 +0300

    Wrote documentation for the `LoadReferencedAssembliesOptions` class

--- TryAtSoftware.Extensions.Reflection/Options/LoadReferencedAssembliesOptions.cs
+++ TryAtSoftware.Extensions.Reflection/Options/LoadReferencedAssembliesOptions.cs
@@ -4,15 +4,27 @@ using System;
 using System.Reflection;
 using TryAtSoftware.Extensions.Reflection.Interfaces;
 
+/// <summary>
+/// A class representing the options that can be used to refine the process of loading referenced assemblies.
+/// </summary>
 public class LoadReferencedAssembliesOptions
 {
     private IAssemblyLoader _loader = new AssemblyLoader();
 
+    /// <summary>
+    /// Gets or sets an instance of the <see cref="IAssemblyLoader"/> interface used to load assemblies.
+    /// </summary>
+    /// <exception cref="ArgumentNullException">Thrown when attempting to set <c>null</c> value.</exception>
     public IAssemblyLoader Loader
     {
         get => this._loader;
         set => this._loader = value ?? throw new ArgumentNullException(nameof(value));
     }
 
+    /// <summary>
+    /// Gets or sets an optional filter function used to control which assemblies should be loaded.
+    /// If the filter determines that a given assembly should not be loaded, none of the assemblies it references (including the referenced assemblies of the referenced assemblies, and so on) will be loaded as well.
+    /// This property is nullable, meaning it can be assigned a value or left as <c>null</c> if no filtering is required.
+    /// </summary>
     public Func<AssemblyName, bool>? RestrictSearchFilter { get; set; }
 }

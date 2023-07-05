d244802ed0303c621e613d8cdca4aa08ac684820
Tony Troeff <tonyslav_troeff@outlook.com>
2023-07-05 22:57:40 +0300

    Wrote tests for the `LoadReferencedAssembliesOptions` class

--- TryAtSoftware.Extensions.Reflection/Options/LoadReferencedAssembliesOptions.cs
+++ TryAtSoftware.Extensions.Reflection/Options/LoadReferencedAssembliesOptions.cs
@@ -7,7 +7,6 @@ using TryAtSoftware.Extensions.Reflection.Interfaces;
 public class LoadReferencedAssembliesOptions
 {
     private IAssemblyLoader _loader = new AssemblyLoader();
-    private Func<AssemblyName, bool> _restrictSearchFilter = _ => true;
 
     public IAssemblyLoader Loader
     {
@@ -15,9 +14,5 @@ public class LoadReferencedAssembliesOptions
         set => this._loader = value ?? throw new ArgumentNullException(nameof(value));
     }
 
-    public Func<AssemblyName, bool>? RestrictSearchFilter
-    {
-        get => this._restrictSearchFilter;
-        set => this._restrictSearchFilter = value ?? throw new ArgumentNullException(nameof(value));
-    }
+    public Func<AssemblyName, bool>? RestrictSearchFilter { get; set; }
 }

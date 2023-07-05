b9a2cc5dc9f96d3d868579d36d8d2adffaef665b
Tony Troeff <tonyslav_troeff@outlook.com>
2023-07-05 23:19:43 +0300

    Documented the `AssemblyExtensions` class.

--- TryAtSoftware.Extensions.Reflection/AssemblyExtensions.cs
+++ TryAtSoftware.Extensions.Reflection/AssemblyExtensions.cs
@@ -6,12 +6,21 @@ using System.Reflection;
 using TryAtSoftware.Extensions.Collections;
 using TryAtSoftware.Extensions.Reflection.Options;
 
+/// <summary>
+/// A static class containing standard extension methods that are useful when executing common operations with assemblies.
+/// </summary>
 public static class AssemblyExtensions
 {
+    /// <summary>
+    /// Use this method to load referenced assemblies, starting from the extended <paramref name="assembly"/>. 
+    /// </summary>
+    /// <param name="assembly">The extended <see cref="Assembly"/> instance.</param>
+    /// <param name="options">The options used to refine the process of loading referenced assemblies. If not provided, a default <see cref="LoadReferencedAssembliesOptions"/> instance will be used.</param>
+    /// <exception cref="ArgumentNullException">Thrown if the extended <paramref name="assembly"/> is <c>null</c>.</exception>
     public static void LoadReferencedAssemblies(this Assembly assembly, LoadReferencedAssembliesOptions? options = null)
     {
         if (assembly is null) throw new ArgumentNullException(nameof(assembly));
-        
+
         LoadReferencedAssemblies(assembly, new HashSet<string>(), options ?? new LoadReferencedAssembliesOptions());
     }
 

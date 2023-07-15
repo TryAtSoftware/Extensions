f37ee29e2bcdbd33d4d8f2bc43e52d26edfa5bd4
Tony Troeff <tonyslav_troeff@outlook.com>
2023-07-15 13:00:01 +0300

    Added an interface declaration of the component responsible for registering services in DI.

---
+++ TryAtSoftware.Extensions.DependencyInjection/Interfaces/IServiceRegistrar.cs
@@ -0,0 +1,14 @@
+﻿namespace TryAtSoftware.Extensions.DependencyInjection.Interfaces;
+
+/// <summary>
+/// An interface defining the structure of a component responsible for registering services into a DI container.
+/// </summary>
+public interface IServiceRegistrar
+{
+    /// <summary>
+    /// Use this method to register a service of the given <paramref name="type"/> into a DI container.
+    /// </summary>
+    /// <param name="type">The <see cref="Type"/> of the service.</param>
+    /// <returns>Returns a value indicating whether the service was successfully registered.</returns>
+    bool Register(Type type);
+}

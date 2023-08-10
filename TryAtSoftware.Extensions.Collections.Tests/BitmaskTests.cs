5eb8ca69b35e6e53b062d6540b605808631f1619
Tony Troeff <tonyslav_troeff@outlook.com>
2023-08-10 22:03:02 +0300

    Implemented the `GetSegment` and `SetSegment` along with the `&` operator.

--- TryAtSoftware.Extensions.Collections.Tests/BitmaskTests.cs
+++ TryAtSoftware.Extensions.Collections.Tests/BitmaskTests.cs
@@ -11,12 +11,13 @@ public class BitmaskTests
     {
         var randomCount = RandomizationHelper.RandomInteger(100, 1000);
         var bitmask = new Bitmask(randomCount, initializeWithZeros: true);
-        
-        Assert.Equal(Math.Ceiling(randomCount / 64.0), bitmask.SegmentsCount);
-        Assert.Equal(randomCount, bitmask.Count);
+
+        AssertDimensions(bitmask, randomCount);
         Assert.True(bitmask.IsZero);
         Assert.False(bitmask.IsOne);
         for (var i = 0; i < randomCount; i++) Assert.False(bitmask.IsSet(i));
+
+        for (var i = 0; i < bitmask.SegmentsCount; i++) Assert.Equal(0, bitmask.GetSegment(i));
     }
 
     [Fact]
@@ -24,12 +25,35 @@ public class BitmaskTests
     {
         var randomCount = RandomizationHelper.RandomInteger(100, 1000);
         var bitmask = new Bitmask(randomCount, initializeWithZeros: false);
-        
-        Assert.Equal(Math.Ceiling(randomCount / 64.0), bitmask.SegmentsCount);
-        Assert.Equal(randomCount, bitmask.Count);
+
+        AssertDimensions(bitmask, randomCount);
         Assert.False(bitmask.IsZero);
         Assert.True(bitmask.IsOne);
         for (var i = 0; i < randomCount; i++) Assert.True(bitmask.IsSet(i));
+
+        for (var i = 0; i < bitmask.SegmentsCount; i++) Assert.Equal(~0, bitmask.GetSegment(i));
+    }
+
+    [Fact]
+    public void SegmentShouldBeModifiedSuccessfully()
+    {
+        var segmentsCount = RandomizationHelper.RandomInteger(10, 100);
+        var randomSegments = new long[segmentsCount];
+        var bitmask = new Bitmask(segmentsCount * Bitmask.BitsPerSegment, initializeWithZeros: true);
+
+        for (var i = 0; i < segmentsCount; i++)
+        {
+            // This is a temporary solution until TryAtSoftware.Randomizer exposes methods to randomize numbers of various types.
+            for (var j = 0; j < 4; j++)
+            {
+                randomSegments[i] = (randomSegments[i] << 16);
+                randomSegments[i] |= (uint)RandomizationHelper.RandomInteger(0, short.MaxValue + 1);
+            }
+            
+            bitmask.SetSegment(i, randomSegments[i]);
+        }
+        
+        for (var i = 0; i < segmentsCount; i++) Assert.Equal(randomSegments[i], bitmask.GetSegment(i));
     }
 
     [Fact]
@@ -41,5 +65,33 @@ public class BitmaskTests
         Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.IsSet(-1 * RandomizationHelper.RandomInteger(1, 100)));
         Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.IsSet(randomCount));
         Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.IsSet(randomCount + RandomizationHelper.RandomInteger(1, 100)));
+    }
+
+    [Fact]
+    public void GetSegmentShouldValidateIndex()
+    {
+        var randomCount = RandomizationHelper.RandomInteger(100, 1000);
+        var bitmask = new Bitmask(randomCount, initializeWithZeros: true);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.GetSegment(-1 * RandomizationHelper.RandomInteger(1, 100)));
+        Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.GetSegment(bitmask.SegmentsCount));
+        Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.GetSegment(bitmask.SegmentsCount + RandomizationHelper.RandomInteger(1, 100)));
+    }
+
+    [Fact]
+    public void SetSegmentShouldValidateIndex()
+    {
+        var randomCount = RandomizationHelper.RandomInteger(100, 1000);
+        var bitmask = new Bitmask(randomCount, initializeWithZeros: true);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.SetSegment(-1 * RandomizationHelper.RandomInteger(1, 100), 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.SetSegment(bitmask.SegmentsCount, 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => bitmask.SetSegment(bitmask.SegmentsCount + RandomizationHelper.RandomInteger(1, 100), 0));
+    }
+
+    private static void AssertDimensions(Bitmask bitmask, int randomCount)
+    {
+        Assert.Equal(Math.Ceiling(randomCount / (double) Bitmask.BitsPerSegment), bitmask.SegmentsCount);
+        Assert.Equal(randomCount, bitmask.Count);
     }
 }
--- TryAtSoftware.Extensions.Collections/Bitmask.cs
+++ TryAtSoftware.Extensions.Collections/Bitmask.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class Bitmask
 {
-    private const int BitsPerElement = 64;
+    public const int BitsPerSegment = 64;
     private readonly List<long> _segments;
 
     public int Count { get; }
@@ -15,7 +15,7 @@ public class Bitmask
 
     public Bitmask(int count, bool initializeWithZeros)
     {
-        var requiredSegmentsCount = Math.DivRem(count, BitsPerElement, out var remainder);
+        var requiredSegmentsCount = Math.DivRem(count, BitsPerSegment, out var remainder);
         if (remainder != 0) requiredSegmentsCount++;
 
         this._segments = new List<long>(capacity: requiredSegmentsCount);
@@ -36,12 +36,46 @@ public class Bitmask
         return (this._segments[segmentIndex] & (1 << bitIndex)) != 0;
     }
 
+    public void SetSegment(int index, long value)
+    {
+        this.ValidateSegmentIndex(index);
+        this._segments[index] = value;
+    }
+
+    public long GetSegment(int index)
+    {
+        this.ValidateSegmentIndex(index);
+        return this._segments[index];
+    }
+
+    public static Bitmask operator &(Bitmask a, Bitmask b)
+    {
+        var result = new Bitmask(count: Math.Max(a.Count, b.Count), initializeWithZeros: false);
+        for (var i = 0; i < result._segments.Count; i++)
+        {
+            var segment = result.GetSegment(i);
+
+            if (i < a.SegmentsCount) segment &= a.GetSegment(i);
+            if (i < b.SegmentsCount) segment &= b.GetSegment(i);
+
+            result.SetSegment(i, segment);
+        }
+
+        return result;
+    }
+
     private (int SegmentIndex, int BitIndex) Locate(int position)
     {
         if (position < 0) throw new ArgumentOutOfRangeException(nameof(position), "Bit position must be a non-negative number.");
         if (position >= this.Count) throw new ArgumentOutOfRangeException(nameof(position), "Bit position must be less than the total number of bits.");
 
-        var segmentIndex = Math.DivRem(position, BitsPerElement, out var bitIndex);
+        var segmentIndex = Math.DivRem(position, BitsPerSegment, out var bitIndex);
         return (segmentIndex, bitIndex);
+    }
+
+    private void ValidateSegmentIndex(int segmentIndex)
+    {
+        if (segmentIndex < 0) throw new ArgumentOutOfRangeException(nameof(segmentIndex), "Segment index must be a non-negative number.");
+        if (segmentIndex >= this.SegmentsCount) throw new ArgumentOutOfRangeException(nameof(segmentIndex), "Segment index must be less than the total number of segments.");
     }
 }
